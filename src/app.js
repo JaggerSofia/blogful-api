6413efb37af5ffa5670eecc64913ef09427d11e7
Victoria Moore <victoriamoore563@gmail.com>
2020-01-18 15:00:18 -0800

    small changes made throughout

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const articlesRouter = require('./articles/articles-router.js')
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -15,8 +15,15 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use('/articles', articlesRouter)
+
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
+    res.send('Hello, world!')
+})
+
+app.get('/xss', (req, res) => {
+    res.cookie('secretToken', '1234567890');
+    res.sendFile(__dirname + '/xss-example.html')
 })
 
 app.use(function errorHandler(error, req, res, next) {
